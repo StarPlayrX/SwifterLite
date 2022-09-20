e8d8672c32531b29a095adf253426573bcbd5462
Todd Bruss <admin@starplayrx.com>
2022-09-19 21:55:26 -0400

    littleEndian BigEndian

--- Sources/SwifterLite/SocketPort.swift
+++ Sources/SwifterLite/SocketPort.swift
@@ -14,12 +14,16 @@ extension Socket {
     public func port() throws -> in_port_t {
         var addr = sockaddr_in()
         return try withUnsafePointer(to: &addr) { pointer in
+            
             var len = socklen_t(MemoryLayout<sockaddr_in>.size)
-            if getsockname(socketFileDescriptor, UnsafeMutablePointer(OpaquePointer(pointer)), &len) != 0 {
+            
+            if getsockname(socketFileDescriptor, UnsafeMutablePointer(OpaquePointer(pointer)), &len) == 0 {
+                let sin_port = pointer.pointee.sin_port
+                return Int(OSHostByteOrder()) != OSLittleEndian ? sin_port.littleEndian : sin_port.bigEndian
+            } else {
                 throw SocketError.getSockNameFailed(ErrNumString.description())
             }
-            let sin_port = pointer.pointee.sin_port
-            return Int(OSHostByteOrder()) != OSLittleEndian ? sin_port.littleEndian : sin_port.bigEndian
+            
         }
     }
 }
